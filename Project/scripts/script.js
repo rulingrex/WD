0dbb9ff2ec3e134c9157059c8c49b048e7ecc338
rulingrex <rulingrex@users.noreply.github.com>
2019-05-15 22:53:55 +0545

    Website

---
+++ Project/scripts/script.js
@@ -0,0 +1,49 @@
+/* Getting running Year it in the footer. */
+document.getElementById("currentYear").innerHTML = footerYear();
+function footerYear() {
+	var d = new Date();
+	return d.getFullYear();
+}
+
+/* Getting Today Date and display on the header */
+document.getElementById("todayDate").innerHTML = todayDate();
+
+function todayDate() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+return months[d.getMonth()]+' '+d.getDate()+', '+d.getFullYear();
+}
+
+
+// SLiders
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+function isNumber(evt) {
+  var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+  if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+    return false;
+
+  return true;
+}    
